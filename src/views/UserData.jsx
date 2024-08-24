98b23c3e29d0fca00fdccebd2334868fe0a775c5
XYZ <xyzscer@outlook.com>
2024-08-24 15:02:28 +0800

    fix: UserData页面用户信息的保存

--- src/views/UserData.jsx
+++ src/views/UserData.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 export default function({lengthOfSongs,lengthOfAlbums}){//TODO:
-    const [userData, setUserData] = useState(/*localStorage.userData??*/{
+    const [userData, setUserData] = useState(localStorage.userData?
+        JSON.parse(localStorage.userData):{
         name: 'Your name',
         description: 'A music listener'
     })
@@ -12,7 +13,8 @@ export default function({lengthOfSongs,lengthOfAlbums}){//TODO:
             <h1 contentEditable onInput={(e)=>{
                 setUserData({...userData,name:e.target.textContent})
             }}>{userData.name}</h1>
-            <p contentEditable>{userData.description}</p>
+            <p contentEditable  onInput={(e)=>{
+                setUserData({...userData,description:e.target.textContent}) }}>{userData.description}</p>
             <p>{lengthOfSongs} songs, {lengthOfAlbums} albums.</p>
         </div>
     )
