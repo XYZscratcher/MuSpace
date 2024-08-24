ae329a876d40efbda1db1e2a5a765b08246f48ce
XYZ <xyzscer@outlook.com>
2024-08-24 14:15:35 +0800

    feat:zh-CN 与 en-US 的部分翻译

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,15 +1,17 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import zhCN from "../locale/zh-CN.json"
+import enUS from "../locale/en-US.json"
 
 await i18next.use(initReactI18next).init({
     lng: navigator.language,
     debug: true,
     resources: {
-        "en": {
-            translation: { "search_placeholder": "Search for songs, albums, artists..." }
+        "en-US": {
+            translation: enUS
         },
         "zh-CN": {
-            translation: { "search_placeholder": "搜索音乐、专辑、艺术家..." }
+            translation: zhCN
         },
     },
     interpolation: {
